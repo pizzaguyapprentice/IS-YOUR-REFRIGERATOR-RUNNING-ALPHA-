62e148cc2e99b49ae5225a6c03981a96a381a723
LqWkWyn <laurence.quinn.work@gmail.com>
2024-11-14 16:27:42 +0000

    Slight changes to program, camera angle ball movement etc
    camera still bugged on respawns

--- Assets/Challenge 4/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -14,7 +14,7 @@ public class PlayerControllerX : MonoBehaviour
 
     private float normalStrength = 10; // how hard to hit enemy without powerup
     private float powerupStrength = 25; // how hard to hit enemy with powerup
-    
+
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
@@ -25,11 +25,14 @@ public class PlayerControllerX : MonoBehaviour
     {
         // Add force to player in direction of the focal point (and camera)
         float verticalInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime); 
+        playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime);
+
+        // Add force for horizontal movement using A and D keys
+        float horizontalInput = Input.GetAxis("Horizontal");
+        playerRb.AddForce(focalPoint.transform.right * horizontalInput * speed * Time.deltaTime);
 
         // Set powerup indicator position to beneath player
         powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
-
     }
 
     // If Player collides with powerup, activate powerup
@@ -40,6 +43,7 @@ public class PlayerControllerX : MonoBehaviour
             Destroy(other.gameObject);
             hasPowerup = true;
             powerupIndicator.SetActive(true);
+            StartCoroutine(PowerupCooldown());
         }
     }
 
@@ -57,21 +61,16 @@ public class PlayerControllerX : MonoBehaviour
         if (other.gameObject.CompareTag("Enemy"))
         {
             Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
-            Vector3 awayFromPlayer =  transform.position - other.gameObject.transform.position; 
-           
+            Vector3 awayFromPlayer = transform.position - other.gameObject.transform.position;
+
             if (hasPowerup) // if have powerup hit enemy with powerup force
             {
                 enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
             }
-            else // if no powerup, hit enemy with normal strength 
+            else // if no powerup, hit enemy with normal strength
             {
                 enemyRigidbody.AddForce(awayFromPlayer * normalStrength, ForceMode.Impulse);
             }
-
-
         }
     }
-
-
-
 }
--- Assets/Challenge 4/Scripts/RotateCameraX.cs
+++ Assets/Challenge 4/Scripts/RotateCameraX.cs
@@ -1,19 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class RotateCameraX : MonoBehaviour
-{
-    private float speed = 200;
-    public GameObject player;
+//public class RotateCameraX : MonoBehaviour
+//{
+//    private float speed = 200;
+//    public GameObject player;
 
     // Update is called once per frame
-    void Update()
-    {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up, horizontalInput * speed * Time.deltaTime);
+//    void Update()
+ //   {
+ //       float horizontalInput = Input.GetAxis("Horizontal");
+  //      transform.Rotate(Vector3.up, horizontalInput * speed * Time.deltaTime);
 
-        transform.position = player.transform.position; // Move focal point with player
+  //      transform.position = player.transform.position; // Move focal point with player
 
-    }
-}
+  // }
+//}
