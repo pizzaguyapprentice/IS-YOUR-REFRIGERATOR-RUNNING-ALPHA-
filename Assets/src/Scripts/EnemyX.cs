8a43b526da556d28166582f2ee54a0c5f0093772
pizzaguyapprentice <matisew8247@gmail.com>
2024-11-15 21:30:48 +0000

    Alpha Commit
    Alpha Version Complete

---
+++ Assets/src/Scripts/EnemyX.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject player;
+    //private GameObject[] all = GameObject.FindGameObjectsWithTag("Enemy");
+    public NavMeshAgent agent;
+    private SpawnManagerX spawnManagerX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+        spawnManagerX = GameObject.Find("Spawn Manager").GetComponent<SpawnManagerX>();
+        speed = spawnManagerX.enemySpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 hitpoint = player.transform.position;
+        agent.SetDestination(hitpoint);
+        // Set enemy direction towards player goal and move there
+        //Vector3 lookDirection = (player.transform.position).normalized;
+        //Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        //enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Player")
+        {   
+            
+            //Destroy(gameObject);
+            Destroy (GameObject.FindWithTag("Enemy"));
+            spawnManagerX.waveCount = 1;
+            spawnManagerX.SpawnEnemyWave(1);
+            spawnManagerX.GenerateSpawnPosition();
+            ResetPlayerPosition();
+        } 
+        else if (other.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    void ResetPlayerPosition ()
+    {
+        player.transform.position = new Vector3(0, 1, -7);
+        player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        player.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
+    }
+
+
+}
---
+++ Assets/src/Scripts/SpawnManagerX.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
+
+    private float spawnRangeX = 15;
+    private float spawnZMin = 15; // set min spawn Z
+    private float spawnZMax = 25; // set max spawn Z
+
+    public int enemyCount;
+    public int waveCount = 1;
+     public float enemySpeed = 50;
+
+
+    public GameObject player; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyCount = GameObject.FindGameObjectsWithTag("Powerup").Length;
+
+        if (enemyCount == 0)
+        {
+            SpawnEnemyWave(waveCount);
+        }
+
+    }
+
+    // Generate random spawn position for powerups and enemy balls
+    public Vector3 GenerateSpawnPosition ()
+    {
+        float xPos = Random.Range(-spawnRangeX, spawnRangeX);
+        float zPos = Random.Range(spawnZMin, spawnZMax);
+        return new Vector3(xPos, 0, zPos);
+    }
+
+
+    public void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        Vector3 powerupSpawnOffset = new Vector3(0, 0, -15); // make powerups spawn at player end
+        // If no powerups remain, spawn a powerup
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(powerupPrefab, GenerateSpawnPosition() + powerupSpawnOffset, powerupPrefab.transform.rotation);
+        }
+        // Spawn number of enemy balls based on wave number
+        for (int i = 0; i < 1; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+        waveCount++;
+        //ResetPlayerPosition(); // put player back at start
+
+    }
+
+    // Move player back to position in front of own goal
+}
